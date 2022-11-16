2577b61fa8a7bbddb56a86a0608895e141c76270
Henrique Zolini <henriquezolini@gmail.com>
2022-11-16 09:15:09 -0300

    Atualização

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Card, Col, Row } from "antd";
+import { Card, Col, Row } from 'antd'
 
 export default function Home() {
   return (
@@ -15,5 +15,5 @@ export default function Home() {
         </Col>
       </Row>
     </>
-  );
+  )
 }
